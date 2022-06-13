e7c58f345f1f335754d87a63ac29e7e7b3e70238
Jon McLean <jmclean@attentivemobile.com>
2022-06-13 13:28:42 -0400

    fix: add images with AWS CLI already baked in

--- build-ubuntu.sh
+++ build-ubuntu.sh
@@ -14,10 +14,24 @@ for PLATFORM in "${PLATFORMS[@]}"; do
     for UBUNTU_VERSION in "${UBUNTU_VERSIONS[@]}"; do
         docker build \
         -f Dockerfile-ubuntu \
+        --build-arg PLATFORM=${PLATFORM} \
         --build-arg UBUNTU_VERSION=${UBUNTU_VERSION} \
         --build-arg BUILD_DATE=$(date +"%Y-%m-%dT%H:%M:%S%z") \
         --build-arg BUILD_REVISION=$(git rev-parse HEAD) \
         --platform="linux/${PLATFORM}" \
         -t carbonnexus/act-images:act-${PLATFORM}-ubuntu-${UBUNTU_VERSION} .
+    done
+done
+
+for PLATFORM in "${PLATFORMS[@]}"; do
+    for UBUNTU_VERSION in "${UBUNTU_VERSIONS[@]}"; do
+        docker build \
+        -f Dockerfile-ubuntu-aws \
+        --build-arg PLATFORM=${PLATFORM} \
+        --build-arg UBUNTU_VERSION=${UBUNTU_VERSION} \
+        --build-arg BUILD_DATE=$(date +"%Y-%m-%dT%H:%M:%S%z") \
+        --build-arg BUILD_REVISION=$(git rev-parse HEAD) \
+        --platform="linux/${PLATFORM}" \
+        -t carbonnexus/act-images:act-${PLATFORM}-ubuntu-aws-${UBUNTU_VERSION} .
     done
 done
--- publish-ubuntu.sh
+++ publish-ubuntu.sh
@@ -13,5 +13,6 @@ PLATFORMS=(
 for PLATFORM in "${PLATFORMS[@]}"; do
     for UBUNTU_VERSION in "${UBUNTU_VERSIONS[@]}"; do
         docker push carbonnexus/act-images:act-${PLATFORM}-ubuntu-${UBUNTU_VERSION}
+        docker push carbonnexus/act-images:act-${PLATFORM}-ubuntu-aws-${UBUNTU_VERSION}
     done
 done

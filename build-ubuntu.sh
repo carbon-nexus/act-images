9852410242f8cfd0aff17c510f3ad456846acffc
Al Wiesner <awiesner@attentivemobile.com>
2023-09-19 14:04:02 -0400

    fix: add new docker image with python

--- build-ubuntu.sh
+++ build-ubuntu.sh
@@ -60,3 +60,16 @@ for PLATFORM in "${PLATFORMS[@]}"; do
         -t carbonnexus/act-images:act-${PLATFORM}-ubuntu-tilt-${UBUNTU_VERSION} .
     done
 done
+
+for PLATFORM in "${PLATFORMS[@]}"; do
+    for UBUNTU_VERSION in "${UBUNTU_VERSIONS[@]}"; do
+        docker build \
+        -f Dockerfile-ubuntu-python \
+        --build-arg PLATFORM=${PLATFORM} \
+        --build-arg UBUNTU_VERSION=${UBUNTU_VERSION} \
+        --build-arg BUILD_DATE=$(date +"%Y-%m-%dT%H:%M:%S%z") \
+        --build-arg BUILD_REVISION=$(git rev-parse HEAD) \
+        --platform="linux/${PLATFORM}" \
+        -t carbonnexus/act-images:act-${PLATFORM}-ubuntu-python-${UBUNTU_VERSION} .
+    done
+done
